fc3be3fca233ee0da32f47d2139dc405c1c1180c
galacticprofessional-cmd <galacticprofessional@gmail.com>
2025-10-14 02:50:20 +0700

    Сделано задание по ЛБ1 (не проверенное на плате) по CMSIS

--- Core/Src/init_LB1.c
+++ Core/Src/init_LB1.c
@@ -19,28 +19,37 @@ void GPIO_Init_Using_My_Macros(void){
 }
 
 void GPIO_Init_CMSIS1(void){
-    // SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIODEN);
 
-    // SET_BIT(GPIOD->MODER, GPIO_MODER_MODER2_0);        //Настройка пина PD2 на вывод, регистр MODER
-    // CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT14);         //Настройка режима работы выxода на push-pull, регистр OTYPER
-    // SET_BIT(GPIOB->OSPEEDR,GPIO_OSPEEDER_OSPEEDR14_0);  //Настройка скорости работы вывода PB14, регистр OSPEEDR
-    // SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);               //Предварительное выключение светодиода, регистр BSR, бит BR7
-    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOGEN);
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIODEN | RCC_AHB1ENR_GPIOGEN | RCC_AHB1ENR_GPIOEEN | RCC_AHB1ENR_GPIOFEN);
 
-    SET_BIT(GPIOB->MODER, GPIO_MODER_MODER14_0);        //Настройка пина PB14 на вывод, регистр MODER
-    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT14);         //Настройка режима работы выxода на push-pull, регистр OTYPER
-    SET_BIT(GPIOB->OSPEEDR,GPIO_OSPEEDER_OSPEEDR14_0);  //Настройка скорости работы вывода PB14, регистр OSPEEDR
-    SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+    SET_BIT     (GPIOF->MODER,      GPIO_MODER_MODER13_0);          //Настройка пина PF13 на вывод, регистр MODER
+    CLEAR_BIT   (GPIOF->OTYPER,     GPIO_OTYPER_OT13);              //Настройка режима работы выxода на push-pull, регистр OTYPER
+    SET_BIT     (GPIOF->OSPEEDR,    GPIO_OSPEEDER_OSPEEDR13_0);     //Настройка скорости работы вывода PF13, регистр OSPEEDR
+    SET_BIT     (GPIOF->BSRR,       GPIO_BSRR_BR13);                //Предварительное выключение светодиода, регистр BSR, бит BR13
 
-    SET_BIT(GPIOD->MODER, GPIO_MODER_MODER14_0);        //Настройка пина PD14 на вывод, регистр MODER
-    CLEAR_BIT(GPIOD->OTYPER, GPIO_OTYPER_OT14);         //Настройка режима работы выxода на push-pull, регистр OTYPER
-    SET_BIT(GPIOD->OSPEEDR,GPIO_OSPEEDER_OSPEEDR14_0);  //Настройка скорости работы вывода PD14, регистр OSPEEDR
-    SET_BIT(GPIOD->BSRR, GPIO_BSRR_BR14);
+    SET_BIT     (GPIOE->MODER,      GPIO_MODER_MODER9_0);          //Настройка пина PE9 на вывод, регистр MODER
+    CLEAR_BIT   (GPIOE->OTYPER,     GPIO_OTYPER_OT9);              //Настройка режима работы выxода на push-pull, регистр OTYPER
+    SET_BIT     (GPIOE->OSPEEDR,    GPIO_OSPEEDER_OSPEEDR9_0);     //Настройка скорости работы вывода PE9, регистр OSPEEDR
+    SET_BIT     (GPIOE->BSRR,       GPIO_BSRR_BR9);                //Предварительное выключение светодиода, регистр BSR, бит BR9
 
-    SET_BIT(GPIOD->MODER, GPIO_MODER_MODER15_0);        //Настройка пина PD15 на вывод, регистр MODER
-    CLEAR_BIT(GPIOD->OTYPER, GPIO_OTYPER_OT15);         //Настройка режима работы выxода на push-pull, регистр OTYPER
-    SET_BIT(GPIOD->OSPEEDR,GPIO_OSPEEDER_OSPEEDR15_0);  //Настройка скорости работы вывода PD15, регистр OSPEEDR
-    SET_BIT(GPIOD->BSRR, GPIO_BSRR_BR15);
+    SET_BIT     (GPIOE->MODER,      GPIO_MODER_MODER11_0);          //Настройка пина PE11 на вывод, регистр MODER
+    CLEAR_BIT   (GPIOE->OTYPER,     GPIO_OTYPER_OT11);              //Настройка режима работы выxода на push-pull, регистр OTYPER
+    SET_BIT     (GPIOE->OSPEEDR,    GPIO_OSPEEDER_OSPEEDR11_0);     //Настройка скорости работы вывода PE11, регистр OSPEEDR
+    SET_BIT     (GPIOE->BSRR,       GPIO_BSRR_BR11);                //Предварительное выключение светодиода, регистр BSR, бит BR11
 
+    SET_BIT     (GPIOF->MODER,      GPIO_MODER_MODER14_0);          //Настройка пина PF14 на вывод, регистр MODER
+    CLEAR_BIT   (GPIOF->OTYPER,     GPIO_OTYPER_OT14);              //Настройка режима работы выxода на push-pull, регистр OTYPER
+    SET_BIT     (GPIOF->OSPEEDR,    GPIO_OSPEEDER_OSPEEDR14_0);     //Настройка скорости работы вывода PF14, регистр OSPEEDR
+    SET_BIT     (GPIOF->BSRR,       GPIO_BSRR_BR14);                //Предварительное выключение светодиода, регистр BSR, бит BR14
+
+    SET_BIT     (GPIOE->MODER,      GPIO_MODER_MODER13_0);          //Настройка пина PE13 на вывод, регистр MODER
+    CLEAR_BIT   (GPIOE->OTYPER,     GPIO_OTYPER_OT13);              //Настройка режима работы выxода на push-pull, регистр OTYPER
+    SET_BIT     (GPIOE->OSPEEDR,    GPIO_OSPEEDER_OSPEEDR13_0);     //Настройка скорости работы вывода PE13, регистр OSPEEDR
+    SET_BIT     (GPIOE->BSRR,       GPIO_BSRR_BR13);                //Предварительное выключение светодиода, регистр BSR, бит BR13
+
+    SET_BIT     (GPIOF->MODER,      GPIO_MODER_MODER15_0);          //Настройка пина PF15 на вывод, регистр MODER
+    CLEAR_BIT   (GPIOF->OTYPER,     GPIO_OTYPER_OT15);              //Настройка режима работы выxода на push-pull, регистр OTYPER
+    SET_BIT     (GPIOF->OSPEEDR,    GPIO_OSPEEDER_OSPEEDR15_0);     //Настройка скорости работы вывода PF15, регистр OSPEEDR
+    SET_BIT     (GPIOF->BSRR,       GPIO_BSRR_BR15);                //Предварительное выключение светодиода, регистр BSR, бит BR15
 
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,24 +2,71 @@
 
 #define TIME_DELAY 1000000
 
+void TurningOnState(void){
+    SET_BIT(GPIOF->BSRR, GPIO_BSRR_BS13);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOE->BSRR, GPIO_BSRR_BS9);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOE->BSRR, GPIO_BSRR_BS11);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOF->BSRR, GPIO_BSRR_BS14);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOE->BSRR, GPIO_BSRR_BS13);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOF->BSRR, GPIO_BSRR_BS15);
+}
+
+void TurningOffState(void){
+    SET_BIT(GPIOF->BSRR, GPIO_BSRR_BR13);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOE->BSRR, GPIO_BSRR_BR9);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOE->BSRR, GPIO_BSRR_BR11);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOF->BSRR, GPIO_BSRR_BR14);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOE->BSRR, GPIO_BSRR_BR13);
+    for(int i = 1; i <= TIME_DELAY; i++){}
+    SET_BIT(GPIOF->BSRR, GPIO_BSRR_BR15);
+}
+int TheSwitcher(int count){
+    int key;
+    if(count % 2 == 0){
+        key = 0;
+    }else{
+        key = 1;
+    }
+    return key;
+}
+
 int main(void){
-        GPIO_Init_CMSIS1();
+    
+    GPIO_Init_CMSIS1();
+    int count = 0;
+
+    while(1){
+        int key = TheSwitcher(count);
         
-        while(1){
+        if(READ_BIT(GPIOG->IDR , GPIO_IDR_IDR_3) != 0){
+            count++;
+        }
+
+        if (key == 0){
             if(READ_BIT(GPIOD->IDR , GPIO_IDR_IDR_2) != 0){
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-                for(int i = 1; i <= TIME_DELAY; i++){}
-                SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS14);
-                for(int i = 1; i <= TIME_DELAY; i++){}
-                SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS15);
+                TurningOnState();
             }
             else if(READ_BIT(GPIOG->IDR , GPIO_IDR_IDR_2) != 0){
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-                for(int i = 1; i <= TIME_DELAY; i++){}
-                SET_BIT(GPIOD->BSRR, GPIO_BSRR_BR14);
-                for(int i = 1; i <= TIME_DELAY; i++){}
-                SET_BIT(GPIOD->BSRR, GPIO_BSRR_BR15);
+                TurningOffState();
             }
-
         }
+        else if(key == 1){
+            if(READ_BIT(GPIOD->IDR , GPIO_IDR_IDR_2) != 0){
+                TurningOffState();
+            }
+            else if(READ_BIT(GPIOG->IDR , GPIO_IDR_IDR_2) != 0){
+                TurningOnState();
+            }            
+        }
+
+    }
 }
